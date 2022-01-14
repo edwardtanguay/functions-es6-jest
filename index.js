38181dfd43581de422c83e51f15fe19fc5d1ba78
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-14 14:17:09 +0100

    first function finished

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
+// npm init -y
 // npm i jest -D
+
 //1
-// const data = { a: 1 };
 const isPlainObject = (potentialObj) => {
-	return typeof(potentialObj) === 'object' && !Array.isArray(potentialObj) && potentialObj !== null;
+	return typeof (potentialObj) === 'object' && !Array.isArray(potentialObj) && potentialObj !== null;
 }
-// console.log(isPlainObject(data)); // true
-// console.log(isPlainObject({a: 2, b: 3})); // true
-// console.log(isPlainObject([1,2,3]));
-// console.log(isPlainObject('test'));
-// console.log(isPlainObject(null));
 
 module.exports = { isPlainObject };
--- index.test.js
+++ index.test.js
@@ -1,2 +1,16 @@
-const { expect } = require('@jest/globals');
+const { expect, test } = require('@jest/globals');
 const { isPlainObject } = require('./index');
+
+test('isPlainObject() is correctly implemented', () => {
+	expect(isPlainObject({ a: 1 })).toBe(true);
+	expect(isPlainObject({ a: 2, a: 3 })).toBe(true);
+	expect(isPlainObject([1,2,3])).toBe(false);
+	expect(isPlainObject(null)).toBe(false);
+	expect(isPlainObject('ksjfskfjsdf')).toBe(false);
+	expect(isPlainObject(12323)).toBe(false);
+	expect(isPlainObject(false)).toBe(false);
+	expect(isPlainObject(true)).toBe(false);
+	expect(isPlainObject(undefined)).toBe(false);
+	expect(isPlainObject([{a:1}, {b:2}])).toBe(false);
+	expect(isPlainObject({})).toBe(true);
+});
