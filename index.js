866efdc85ca947e63775f991787762900b83df9b
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-14 13:36:23 +0100

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+// npm i jest -D
 //1
-const data = { a: 1 };
-//write your code here 
-console.log(isPlainObject(data)); // true
+// const data = { a: 1 };
+const isPlainObject = (potentialObj) => {
+	return typeof(potentialObj) === 'object' && !Array.isArray(potentialObj) && potentialObj !== null;
+}
+// console.log(isPlainObject(data)); // true
+// console.log(isPlainObject({a: 2, b: 3})); // true
+// console.log(isPlainObject([1,2,3]));
+// console.log(isPlainObject('test'));
+// console.log(isPlainObject(null));
+
+module.exports = { isPlainObject };
---
+++ index.test.js
@@ -0,0 +1,2 @@
+const { expect } = require('@jest/globals');
+const { isPlainObject } = require('./index');
